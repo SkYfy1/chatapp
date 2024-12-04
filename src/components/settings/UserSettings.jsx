edad63a093d62c3ca8d4a6dc7f4adb66c9849040
SkYfy1 <festik.tawer@gmail.com>
2024-12-05 01:37:15 +0200

    form

--- src/components/settings/UserSettings.jsx
+++ src/components/settings/UserSettings.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './userSettings.css'
 import { useAuthStore } from '../../context/useAuthStore';
+import UserForm from './form/UserForm';
 
 const UserSettings = ({ close }) => {
     const userInfo = useAuthStore((state) => state.currentUser);
+    const [changeData, setChangeData] = useState(false);
+
+
+    if (changeData) {
+        return (
+                <UserForm />
+        )
+    };
+
     return (
         <div className='settings'>
             <div className='top'>
@@ -14,7 +24,7 @@ const UserSettings = ({ close }) => {
                     <h2>Settings</h2>
                 </div>
                 <div className='action'>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <svg onClick={() => setChangeData(prev => !prev)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
                     </svg>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
---
+++ src/components/settings/form/UserForm.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useRef } from 'react'
+import './userForm.css'
+import { useForm, } from 'react-hook-form'
+import { useAuthStore } from '../../../context/useAuthStore'
+
+const UserForm = () => {
+    const userData = useAuthStore(state => state.currentUser);
+    const {
+        register,
+        handleSubmit,
+        formState
+    } = useForm({
+        defaultValues: {
+            username: userData.username,
+            phone: userData?.phone,
+            about: userData?.about
+        }
+    });
+
+    const onSubmit = (data) => {
+        alert(JSON.stringify(data));
+    };
+
+    useEffect(() => {
+        console.log(formState.defaultValues)
+    }, [formState])
+
+    return (
+        <div className='userForm'>
+            <div className='top'>
+                <div className='back'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M7.49 12 3.74 8.248m0 0 3.75-3.75m-3.75 3.75h16.5V19.5" />
+                    </svg>
+                    <h2>Change Profile</h2>
+                </div>
+            </div>
+            <div className="imgChange">
+                <div className='wrapper'>
+                    <div className="background">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
+                        </svg>
+                    </div>
+                    <img src={userData.avatar || './avatar.png'} alt="avatar" />
+                </div>
+            </div>
+            <form className="form" id='form' onSubmit={handleSubmit(onSubmit)}>
+                <div className='formInput'>
+                    <label className={formState.defaultValues.username && 'isDirty'} htmlFor="name">Name (Necessarily)</label>
+                    <input id='name' type="text" {...register('username')} />
+                </div>
+                <div className='formInput'>
+                    <label className={formState.dirtyFields.phone && 'isDirty'} htmlFor="phone">Phone (Necessarily)</label>
+                    <input id='phone' type="text" {...register('phone', {
+                        pattern: /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/
+                    })} />
+                </div>
+                <div className='formInput'>
+                    <label className={(formState.dirtyFields.about || formState.defaultValues.about) && 'isDirty'} htmlFor="about">About (Unnecessarily)</label>
+                    <input id='about' type="text" {...register('about')} />
+                </div>
+            </form>
+            <button type='submit' form='form' className={(formState.dirtyFields.username || formState.dirtyFields.about || formState.dirtyFields.phone) && 'showButton'}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                </svg>
+            </button>
+        </div>
+    )
+}
+
+export default UserForm
