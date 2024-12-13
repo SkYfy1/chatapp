ec6da9fe10e97cd6f97e9b069ff1942d1797299e
SkYfy1 <festik.tawer@gmail.com>
2024-12-14 00:31:09 +0200

    lang

---
+++ src/utils/language.js
@@ -0,0 +1,26 @@
+const language = {
+    settings: {
+        information: {
+            ru: ["Номер телефона",
+                "Имя пользователя",
+                "Про меня"],
+            en: ["Phone number",
+                "User name",
+                "About"]
+        },
+        settingsList: {
+            ru: ["Все настройки",
+                "Настройки анимации",
+                "Уведомления",
+                "Данные и память",
+                "Язык"],
+            en: ["All settings",
+                "Animation settings",
+                "Notifications",
+                "Data and memory",
+                "Language"]
+        }
+    }
+}
+
+export default language;
